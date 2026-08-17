0a937670bb2b15c4ab0c6fd98a578ffd6d43937c
Abhicygnus19 <abhijitpaul.cygnus@gmail.com>
2026-08-17 11:40:16 +0530

    17-8-26commit

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // base: "/",
+  // base: "/ird-dashboard",
   base: "/iitd/react-dashboard/",
   // base: "/iitd-rnd-dashboard-demo/v2/",
 });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/",
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "https://cygnusweb.in/iitd",
-//         changeOrigin: true,
-//         secure: false,
-//       },
-//     },
-//   },
-// });
